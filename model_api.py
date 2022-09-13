6a1dce4635ee60dd1ac6b75f5d1c80dac418c53a
Yazan Amer <yazan.amer@progressoft.com>
2022-09-13 12:25:18 +0300

    Automatic checker.

--- model_api.py
+++ model_api.py
@@ -19,5 +19,15 @@ def index():
     return xml_handler.get_content_string()
 
 
+def assert_model_is_functional():
+    x, y = test.prepare_x_y()
+    predictions = predictor.predict(x)
+    r2, mse, mae = test.get_metrics(predictions, y)
+    assert r2 > 0.93
+    assert mse < 0.06
+    assert mae < 0.11
+
+
 if __name__ == "__main__":
+    assert_model_is_functional()
     app.run(debug=True, port=5050)
