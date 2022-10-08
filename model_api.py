37ab280b626a155223a2f32a0580846f8e65a79b
Yazan Amer <yazan.amer@progressoft.com>
2022-10-08 13:41:20 +0300

    request_adapter.py almost done!

--- model_api.py
+++ model_api.py
@@ -11,22 +11,5 @@ def index():
     return service.predict(request_as_raw_xml)
 
 
-def end_to_end_test():
-    def get_sample_BikeCadXml():
-        with open("resources/BikeCADXmlRequest.xml", "r") as file:
-            return file.read()
-
-    assert service.predict(get_sample_BikeCadXml()) == {'Model Mass': -0.9461116790771484,
-                                                        'Sim 1 Bottom Bracket X Disp.': 0.02232583984732628,
-                                                        'Sim 1 Bottom Bracket Y Disp.': 0.2731778919696808,
-                                                        'Sim 1 Dropout X Disp.': 0.09372919797897339,
-                                                        'Sim 1 Dropout Y Disp.': 0.1128099337220192,
-                                                        'Sim 1 Safety Factor': -0.8752062320709229,
-                                                        'Sim 2 Bottom Bracket Z Disp.': 1.7482761144638062,
-                                                        'Sim 3 Bottom Bracket X Rot.': 2.0954513549804688,
-                                                        'Sim 3 Bottom Bracket Y Disp.': 3.2179315090179443,
-                                                        'Sim 3 Safety Factor': -0.3395128548145294}
-
-
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
