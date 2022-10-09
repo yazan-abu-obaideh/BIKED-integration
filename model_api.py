2607ab47eec66cbdf07306ec05e7163226dd2a9f
Yazan Amer <yazan.amer@progressoft.com>
2022-10-09 09:02:21 +0300

    refactor autogluon_service.py

--- model_api.py
+++ model_api.py
@@ -8,7 +8,7 @@ service = AutogluonService()
 @app.route("/")
 def index():
     request_as_raw_xml = request.data.decode("utf-8")
-    return service.predict(request_as_raw_xml)
+    return service.predict_from_xml(request_as_raw_xml)
 
 
 if __name__ == "__main__":
