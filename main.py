9b41161acb8945cc898760f74b13676196c90c79
Yazan Amer <yazan.amer@progressoft.com>
2022-11-03 13:45:31 +0300

    main.py production ready?

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ def index():
 if __name__ == "__main__":
     logger = logging.getLogger('waitress')
     logger.setLevel(logging.INFO)
-    waitress.serve(app, host="0.0.0.0", port=5000)
+    waitress.serve(app, host="0.0.0.0", port=80)
