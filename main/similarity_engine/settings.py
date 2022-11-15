24e54ade077efec47ab522834c16cdb92069f9a1
Yazan Amer <yazan.amer@progressoft.com>
2022-11-15 21:37:47 +0300

    sim-engine

--- main/similarity_engine/settings.py
+++ main/similarity_engine/settings.py
@@ -1,3 +1,8 @@
-# TODO: categories of vars --> some need to be one-hot-encoded,
+# TODO: categories of vars --> some need to be one-hot-encoded, scaled,
 #  names in dataset & names in BikeCAD files
 # we might want to return multiple similar bikes WITH scores (80% similar)
+
+# two broad directions:
+# * performance optimization (time vs accuracy) [been done, literature review needed]
+# * human - AI collaboration (user data?)
+# Multi-fidelity -- possible with a human in the loop
