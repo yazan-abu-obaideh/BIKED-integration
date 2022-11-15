32d6affa574158e7a0f3806d9a3477f0a30bdd6d
Yazan Amer <yazan.amer@progressoft.com>
2022-11-15 21:15:35 +0300

    sim-engine

---
+++ main/similarity_engine/settings.py
@@ -0,0 +1,3 @@
+# TODO: categories of vars --> some need to be one-hot-encoded,
+#  names in dataset & names in BikeCAD files
+# we might want to return multiple similar bikes WITH scores (80% similar)
