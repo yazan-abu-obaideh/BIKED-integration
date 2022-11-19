20d965598c6e495666dc46712022fede7da12588
Yazan Amer <yazan.amer@progressoft.com>
2022-11-19 12:59:31 +0300

    install-correct-py-version.sh

--- install-correct-py-version.sh
+++ install-correct-py-version.sh
@@ -5,3 +5,5 @@ sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.8-venv
 python3.8 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
