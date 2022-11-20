dd2fb8db1bce726406d714e487857186438e1d3a
Yazan Amer <yazan.amer@progressoft.com>
2022-11-20 10:10:31 +0300

    distance_service.py closest n

--- main/recommendation_tools/distance_service.py
+++ main/recommendation_tools/distance_service.py
@@ -14,6 +14,19 @@ class DistanceService:
 
     def get_closest_to(self, user_entry_dict):
 
+        self.calculate_distances(user_entry_dict)
+        smallest_distance = min(self.data[DISTANCE].values)
+
+        response = self.get_response_by_distance(smallest_distance)
+
+        self.remove_distance_column()
+
+        return response
+
+    def remove_distance_column(self):
+        self.data.drop(columns=DISTANCE, axis=1, inplace=True)
+
+    def calculate_distances(self, user_entry_dict):
         self.data: pd.DataFrame
         user_entry_row = pd.Series(user_entry_dict)
 
@@ -21,11 +34,14 @@ class DistanceService:
             return self.get_distance_between(row, user_entry_row)
 
         self.data[DISTANCE] = self.data.apply(distance_from_user_entry, axis=1)
-        smallest_distance = min(self.data[DISTANCE].values)
 
+    def get_response_by_distance(self, smallest_distance):
         correct_row_index = self.data[self.data[DISTANCE] == smallest_distance].index[0]
-        response = pd_util.get_dict_from_row(self.data[self.data.index == correct_row_index])
+        return pd_util.get_dict_from_row(self.data[self.data.index == correct_row_index])
 
-        self.data.drop(columns=DISTANCE, axis=1, inplace=True)
-
-        return response
+    def get_closest_n(self, user_entry, n):
+        self.calculate_distances(user_entry)
+        smallest_n = sorted(self.data[DISTANCE].values)[:n]
+        responses = [self.get_response_by_distance(distance) for distance in smallest_n]
+        self.remove_distance_column()
+        return responses
--- test/test_recommendation_tools/test_distance_service.py
+++ test/test_recommendation_tools/test_distance_service.py
@@ -19,24 +19,36 @@ class TestDistanceService(unittest.TestCase):
 
         self.assertRaises(ValueError, get_closest_to_invalid)
 
+    def test_get_closest_n(self):
+        user_entry = {"x": 1, "y": 1, "z": 1}
+        first_response, second_response = self.service.get_closest_n(user_entry, n=2)
+        self.assertCorrectMatch(self.get_by_index(3),
+                                first_response, expected_distance=0)
+        self.assertCorrectMatch(self.get_by_index(2),
+                                second_response, expected_distance=0.4330127018922193)
+
     def test_get_distance_between(self):
         assert self.service.get_distance_between({"x": 0, "y": 0}, {"x": 3, "y": 4}) == 5
 
     def test_can_get_distance_from_point(self):
-        first = self.get_by_index(min)
-        last = self.get_by_index(max)
+        first = self.get_by_selection_function(min)
+        last = self.get_by_selection_function(max)
         first_user_entry = {"x": 0.1, "y": 0.05, "z": 0.03}
         second_user_entry = {"x": 0.95, "y": 0.95, "z": 0.99}
-        self.assertCorrectMatch(first, first_user_entry, expected_distance=0.11575836902790226)
-        self.assertCorrectMatch(last, second_user_entry, expected_distance=0.07141428428542856)
+        self.assertCorrectMatch(first, self.service.get_closest_to(first_user_entry),
+                                expected_distance=0.11575836902790226)
+        self.assertCorrectMatch(last, self.service.get_closest_to(second_user_entry),
+                                expected_distance=0.07141428428542856)
 
-    def get_by_index(self, selection_function):
+    def get_by_selection_function(self, selection_function):
         indices = self.dataset.index.values
         row = self.dataset[self.dataset.index == selection_function(indices)]
         return pd_util.get_dict_from_row(row)
 
-    def assertCorrectMatch(self, row, user_entry, expected_distance):
-        response = self.service.get_closest_to(user_entry)
+    def get_by_index(self, index):
+        return pd_util.get_dict_from_row(self.dataset[self.dataset.index == index])
+
+    def assertCorrectMatch(self, row, response, expected_distance):
         for entry in row.keys():
             assert row[entry] == response[entry]
         assert response[DISTANCE] == expected_distance
