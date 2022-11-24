ac33d7c8286f0283d4582797bce973b549ba1174
Yazan Amer <yazan.amer@progressoft.com>
2022-11-24 15:30:10 +0300

    unscaling...

--- main/request_adapter/request_scaler.py
+++ main/request_adapter/request_scaler.py
@@ -2,30 +2,22 @@ import pandas as pd
 import main.pandas_utility as pd_util
 
 
-def OH_encode(data):
-    data = data.copy()
-    # One-hot encode the materials
-    data.loc[:, "Material"] = pd.Categorical(data["Material"], categories=["Steel", "Aluminum", "Titanium"])
-    mats_oh = pd.get_dummies(data["Material"], prefix="Material=", prefix_sep="")
-    data.drop(["Material"], axis=1, inplace=True)
-    data = pd.concat([mats_oh, data], axis=1)
-    return data
-
-
 class RequestScaler:
 
     def __init__(self, scaler):
         self.scaler = scaler
 
-    def scale(self, row):
+    def scale(self, data):
+        data = pd_util.get_row_from_dict(data)
 
-        row = pd_util.get_row_from_dict(row)
+        x_reg_sc = self.scaler.transform(data)
+        x_reg = pd.DataFrame(x_reg_sc, columns=data.columns, index=data.index)
 
-        x_reg = row.iloc[:, :-11]
+        return pd_util.get_dict_from_row(x_reg)
 
-        x_reg = OH_encode(x_reg)
-
-        x_reg_sc = self.scaler.transform(x_reg)
-        x_reg = pd.DataFrame(x_reg_sc, columns=x_reg.columns, index=x_reg.index)
-
-        return x_reg.iloc[0].to_dict()
+    def unscale(self, data):
+        data = pd_util.get_row_from_dict(data)
+        unscaled_values = self.scaler.inverse_transform(data)
+        return pd_util.get_dict_from_row(pd.DataFrame(unscaled_values,
+                                                      columns=data.columns,
+                                                      index=data.index))
--- test/test_request_adapter/test_request_scaler.py
+++ test/test_request_adapter/test_request_scaler.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main.load_data import load_framed_dataset
+from main.load_data import load_framed_dataset, OH_encode
 from main.request_adapter.request_scaler import RequestScaler
 import os
 import pandas as pd
@@ -14,12 +14,26 @@ class TestRequestScaler(unittest.TestCase):
 
     def setUp(self) -> None:
         self.raw_data = pd.read_csv(os.path.join(RESOURCES_FILE, RELATIVE_PATH), index_col=0)
-        self.scaled_data, _, _, self.scaler = load_framed_dataset("r", True, True, True)
-        self.request_scaler = RequestScaler(self.scaler)
+        self.scaled_data, _, _, scaler = load_framed_dataset("r", True, True, True)
+        self.request_scaler = RequestScaler(scaler)
+        self.input_row = self.prepare_input_row()
+        self.first_scaled = self.scaled_data.iloc[0].to_dict()
 
     def test_identical_to_load_data_scaler(self):
-        first_scaled = self.scaled_data.iloc[0].to_dict()
+        assert type(self.input_row) is pd.DataFrame
+        input_dict = pd_util.get_dict_from_row(self.input_row)
+        assert self.first_scaled == self.request_scaler.scale(input_dict)
+
+    def test_unscaling_works(self):
+        scaled_once = self.request_scaler.scale(pd_util.get_dict_from_row(self.input_row))
+        unscaled_once = self.request_scaler.unscale(scaled_once)
+        unscaled_twice = self.request_scaler.unscale(self.request_scaler.scale(unscaled_once))
+        scaled_twice = self.request_scaler.scale(unscaled_twice)
+        for key in self.first_scaled.keys():
+            self.assertAlmostEqual(scaled_twice[key], self.first_scaled[key])
+
+    def prepare_input_row(self):
         input_row = self.raw_data[:1]
-        assert type(input_row) is pd.DataFrame
-        input_dict = pd_util.get_dict_from_row(input_row)
-        assert first_scaled == self.request_scaler.scale(input_dict)
+        input_row = input_row.iloc[:, :-11]
+        input_row = OH_encode(input_row)
+        return input_row
