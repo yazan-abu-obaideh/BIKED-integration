8fea2396da4949f23f535cd5221128ccb74bf819
Yazan Amer <yazan.amer@progressoft.com>
2022-11-27 17:31:32 +0300

    recommendation_service_settings.py

--- main/recommendation/recommendation_service_settings.py
+++ main/recommendation/recommendation_service_settings.py
@@ -1,8 +1,14 @@
 from abc import abstractmethod
 
+from typing import List
+
 
 class RecommendationSettings:
 
     @abstractmethod
     def max_n(self) -> int:
+        pass
+
+    @abstractmethod
+    def weights(self) -> List[float]:
         pass
