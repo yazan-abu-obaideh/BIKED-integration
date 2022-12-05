ffb5b1a9b35b3924f14a701e87a33d5ef27501fe
Yazan Amer <yazan.amer@progressoft.com>
2022-12-05 14:23:57 +0300

    test_flask_app.py

--- test/test_app/test_flask_app.py
+++ test/test_app/test_flask_app.py
@@ -6,13 +6,9 @@ from app import app
 from requests import request as send_request
 from time import sleep
 
+
 class AppTest(unittest.TestCase):
-
     APP_PROCESS = None
-
-    @staticmethod
-    def run_app():
-        app.run(port=5000)
 
     def test_empty_request(self):
         response = send_request("GET", "http://localhost:5000/evaluate")
@@ -26,13 +22,17 @@ class AppTest(unittest.TestCase):
         self.assertIs(type(response.json()), dict)
         self.assertEqual(200, response.status_code)
 
-
     @classmethod
     def setUpClass(cls) -> None:
         AppTest.APP_PROCESS = multiprocessing.Process(target=AppTest.run_app)
         AppTest.APP_PROCESS.start()
         while send_request("GET", "http://localhost:5000/health").text != "UP":
             sleep(0.5)
+
+    @staticmethod
+    def run_app():
+        app.run(port=5000)
+
     @classmethod
     def tearDownClass(cls) -> None:
         AppTest.APP_PROCESS.terminate()
