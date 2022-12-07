f352d9ce81b3b3d26233661bc942d953ceda61bb
Yazan Amer <yazan.amer@progressoft.com>
2022-12-07 08:35:09 +0300

    recommendation_service.py - now with weights!

--- main/recommendation/recommendation_service.py
+++ main/recommendation/recommendation_service.py
@@ -16,6 +16,20 @@ class RecommendationService:
         #  the settings, the scaling and unscaling required - because this service CAN be concrete.
         #  Do this right and you won't have to change anything about this package if the dataset changes.
 
+    def calculate_distances(self, user_entry_dict):
+        self.data: pd.DataFrame
+        user_entry_row = pd.Series(user_entry_dict)
+
+        def distance_from_user_entry(row):
+            return self.get_distance_between(row, user_entry_row)
+
+        self.data[DISTANCE] = self.data.apply(distance_from_user_entry, axis=1)
+
+    def get_response_by_distance(self, smallest_distance) -> dict:
+        correct_row_index = self.data[self.data[DISTANCE] == smallest_distance].index[0]
+        return pd_util.get_dict_from_row(self.data[self.data.index == correct_row_index])
+
+
     def get_distance_between(self, first_entry, second_entry):
         try:
             deltas = self.get_deltas(first_entry, second_entry)
@@ -66,19 +80,6 @@ class RecommendationService:
 
     def remove_distance_column(self):
         self.data.drop(columns=DISTANCE, axis=1, inplace=True)
-
-    def calculate_distances(self, user_entry_dict):
-        self.data: pd.DataFrame
-        user_entry_row = pd.Series(user_entry_dict)
-
-        def distance_from_user_entry(row):
-            return self.get_distance_between(row, user_entry_row)
-
-        self.data[DISTANCE] = self.data.apply(distance_from_user_entry, axis=1)
-
-    def get_response_by_distance(self, smallest_distance) -> dict:
-        correct_row_index = self.data[self.data[DISTANCE] == smallest_distance].index[0]
-        return pd_util.get_dict_from_row(self.data[self.data.index == correct_row_index])
 
     def raise_if_invalid_number(self, n):
         if n > self.settings.max_n():
