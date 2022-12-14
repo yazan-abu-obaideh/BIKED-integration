584bee5e0a185650033db40f2584c7683cda9ba0
Yazan Amer <yazan.amer@progressoft.com>
2022-12-14 08:11:49 +0300

    bike_recommendation_service.py - microBIKED

--- main/recommendation/bike_recommendation_service.py
+++ main/recommendation/bike_recommendation_service.py
@@ -5,26 +5,62 @@ import pandas as pd
 import os
 
 
-
 class DefaultBikeSettings(RecommendationSettings):
     def __init__(self):
-        self._list = ['upper head tube ext', 'seat tube ext', 'lower head tube ext', 'Rear wheel width', 'Front wheel width',
-                 'Head tube type', 'BB shell length', 'Head tube diam', 'Seat tube wheel cut', 'BB diameter',
-                 'Seat tube diam', 'Top tube type', 'Chain Stay Bridge Diameter', 'Chain stay bridge to rear axle',
-                 'seat stay bridge diam', 'seat stay bridge to rear axle', 'Seattube bottle?', 'Downtube bottle?',
-                 'front fender?', 'rear fender?', 'racks?', 'BB drop', 'seat tube length c-t', 'stack', 'seat angle',
-                 'chain stay length', 'front center distance', 'head angle', 'saddle height', 'Head tube length',
-                 'Rear ERD', 'MTB/Road/Track', 'Front BSD', 'Front ERD', 'Rear BSD', 'Fork Type', 'Type of stem',
-                 'Aerobars?', 'Handlebar style', 'Chain stay bridge?', 'Seat stay bridge?', 'Waterbottles?', 'Chain?',
-                 '# cogs', '# chainrings']
-        self.maybe = ["Head tube upper extension2", "Seat tube extension2", "Head tube lower extension2", "Wheel width rear", "Wheel width front", "Head tube type", "BB length", "Head tube diameter", "Wheel cut", "BB diameter", "Seat tube diameter", "Top tube type", "CHAINSTAYbrdgdia1", "CHAINSTAYbrdgshift", "SEATSTAYbrdgdia1", "SEATSTAYbrdgshift", "bottle SEATTUBE0 show", "bottle DOWNTUBE0 show", "Front Fender include", "Rear Fender include", "Display RACK"]
-        self.yes = ["BB textfield", "Seat tube length", "Stack", "Seat angle", "CS textfield", "FCD textfield", "Head angle", "Saddle height", "Head tube length textfield", "ERD rear", "Dropout spacing style", "BSD front", "ERD front", "BSD rear", "Fork type", "Stem kind", "Display AEROBARS", "Handlebar style", "CHAINSTAYbrdgCheck", "SEATSTAYbrdgCheck", "Display WATERBOTTLES", "BELTorCHAIN", "Number of cogs", "Number of chainrings"]
+        self.maybe = [
+            "upper head tube ext",
+            "seat tube ext",
+            "lower head tube ext",
+            "Rear wheel width",
+            "Front wheel width",
+            "Head tube type",
+            "BB shell length",
+            "Head tube diam",
+            "Seat tube wheel cut",
+            "BB diameter",
+            "Seat tube diam",
+            "Top tube type",
+            "Chain Stay Bridge Diameter",
+            "Chain stay bridge to rear axle",
+            "seat stay bridge diam",
+            "seat stay bridge to rear axle",
+            "Seattube bottle?",
+            "Downtube bottle?",
+            "front fender?",
+            "rear fender?",
+            "racks?"
+        ]
+        self.yes = ["BB drop",
+                    "seat tube length c-t",
+                    "stack",
+                    "seat angle",
+                    "chain stay length",
+                    "front center distance",
+                    "head angle",
+                    "saddle height",
+                    "Head tube length",
+                    "Rear ERD",
+                    "MTB/Road/Track",
+                    "Front BSD",
+                    "Front ERD",
+                    "Rear BSD",
+                    "Fork Type",
+                    "Type of stem",
+                    "Aerobars?",
+                    "Handlebar style",
+                    "Chain stay bridge?",
+                    "Seat stay bridge?",
+                    "Waterbottles?",
+                    "Chain?",
+                    "# cogs",
+                    "# chainrings"
+                    ]
 
     def max_n(self) -> int:
         return 10
 
     def include(self) -> list:
-        return self._list
+        return [value.strip().lower() for value in self.maybe + self.yes]
 
     def weights(self) -> dict:
         maybe_weights = {key: 1 for key in self.maybe}
@@ -33,8 +69,9 @@ class DefaultBikeSettings(RecommendationSettings):
         weights.update(yes_weights)
         return weights
 
+
 DEFAULT_SETTINGS = DefaultBikeSettings()
-DEFAULT_DATASET = os.path.join(os.path.dirname(__file__), "../../resources/datasets/BIKED_recommend.csv")
+DEFAULT_DATASET = os.path.join(os.path.dirname(__file__), "../../resources/datasets/microBIKED_processed.csv")
 
 
 class BikeRecommendationService:
@@ -43,12 +80,13 @@ class BikeRecommendationService:
         self.xml_handler = XmlHandler()
         num_not_included = 0
         for key in self.inner_service.settings.include():
-            if key not in self.inner_service.data.columns.values:
+            if key not in [value.lower() for value in list(self.inner_service.data.columns.values)]:
                 print(key)
                 num_not_included += 1
         print(f"{num_not_included=}")
-        num_included = len(self.inner_service.settings.include())
-        print(f"{num_included=}")
+        max_num_included = len(self.inner_service.settings.include())
+        print(f"{max_num_included=}")
+
     def recommend_bike(self, xml_user_entry: str):
         self.xml_handler.set_xml(xml_user_entry)
         user_entry_dict = self.xml_handler.get_entries_dict()
@@ -71,6 +109,6 @@ class BikeRecommendationService:
                 self.warning[key] = value
                 return 0
 
+
 if __name__ == "__main__":
     service = BikeRecommendationService()
-
