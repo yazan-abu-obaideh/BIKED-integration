5bc5c1d417a32e56661bd9132d3e032766eb7ab2
Yazan Amer <yazan.amer@progressoft.com>
2022-12-27 21:16:44 +0300

    test.sh fix -___-

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 source venv/bin/activate
-python -m unittest discover -s /home/yazan/Repositories/Personal/BikeCAD-integration/test -t /home/yazan/Repositories/Personal/BikeCAD-integration/test
+python -m unittest discover -s ./test -t ./test
