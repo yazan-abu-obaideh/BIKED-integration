576430ffa039a6cc45cb81c554a4a2ab05aa6a5e
Yazan Amer <yazan.amer@progressoft.com>
2023-01-05 14:03:48 +0300

    robot_qa.py

--- acceptance_tests/robot_qa.py
+++ acceptance_tests/robot_qa.py
@@ -11,6 +11,7 @@ class Relationship:
 class RobotQaDepartment(unittest.TestCase):
     def __init__(self, processing_function):
         super().__init__()
+        self.reported_errors = []
         self.processing_function = processing_function
         self.base_request = None
         self.proportional_relationships = []
@@ -23,8 +24,27 @@ class RobotQaDepartment(unittest.TestCase):
         self.inverse_relationships.append(relationship)
 
     def execute_assertions(self):
-        self.__execute_for(self.proportional_relationships, self.assertGreater)
-        self.__execute_for(self.inverse_relationships, self.assertLess)
+        self.__execute_for(self.proportional_relationships, self.report_assertGreater)
+        self.__execute_for(self.inverse_relationships, self.report_assertLess)
+
+    def report_assertGreater(self, a, b):
+        self.report_assert(self.assertGreater, a, b)
+
+    def report_assertLess(self, a, b):
+        self.report_assert(self.assertLess, a, b)
+
+    def report_assert(self, assertion_function, a, b):
+        try:
+            assertion_function(a, b)
+        except AssertionError as error:
+            self.reported_errors.append(error)
+
+    def get_request(self):
+        mutable_request = copy.deepcopy(self.base_request)
+        return mutable_request
+
+    def set_request(self, base_request):
+        self.base_request = base_request
 
     def __execute_for(self, relationships: list, assertion_function: callable):
         for relationship in relationships:
@@ -37,13 +57,6 @@ class RobotQaDepartment(unittest.TestCase):
                 old_response = self.processing_function(base_request)
                 base_request[key] += 1
                 assertion_function(self.processing_function(base_request)[value], old_response[value])
-
-    def get_request(self):
-        mutable_request = copy.deepcopy(self.base_request)
-        return mutable_request
-
-    def set_request(self, base_request):
-        self.base_request = base_request
 
 
 class RobotQaTest(unittest.TestCase):
@@ -70,11 +83,12 @@ class RobotQaTest(unittest.TestCase):
     def test_qa_passes(self):
         self.robot_qa.execute_assertions()
 
-    def test_qa_catches_errors(self):
+    def test_qa_does_not_throw(self):
         self.robot_qa.processing_function = lambda x: {"sum": x['a1'] + x['a2'] + x['d2'],
                                                        "division": (x['d2'] + x['a1']) / (x['d2'] + x['a2'])}
-        with self.assertRaises(expected_exception=AssertionError) as context:
-            self.robot_qa.execute_assertions()
+        self.assertEqual(len(self.robot_qa.reported_errors), 0)
+        self.robot_qa.execute_assertions()
+        self.assertEqual(len(self.robot_qa.reported_errors), 2)
 
     def request(self):
         return {
