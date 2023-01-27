94de8ea04e3d901302d68818bca5d06d64d408f0
Yazan Amer <yazan.amer@progressoft.com>
2023-01-27 12:46:09 +0300

    bike_recommendation_service.py

--- test/test_recommendation/test_bike_recommendation_service.py
+++ test/test_recommendation/test_bike_recommendation_service.py
@@ -9,6 +9,7 @@ VALID_MODEL_PATH = os.path.join(os.path.dirname(__file__), "../resources/bikes/F
 class RecommendationServiceTest(unittest.TestCase):
     def setUp(self) -> None:
         self.service = BikeRecommendationService()
+        self.xml_handler = XmlHandler()
 
     def test_get_closest_to(self):
         with open(VALID_MODEL_PATH, 'r') as file:
@@ -17,6 +18,5 @@ class RecommendationServiceTest(unittest.TestCase):
     def test_grab_bike_file(self):
         bike_file = self.service.grab_bike_file(1)
         self.xml_handler.set_xml(bike_file)
-        print(self.xml_handler.get_entries_dict())
         self.assertGreater(len(self.xml_handler.get_entries_dict()), 25)
 
