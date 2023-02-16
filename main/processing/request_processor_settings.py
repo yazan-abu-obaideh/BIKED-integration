600fdf5757ba6b36b77e585a04d7ac3b44e96a61
Yazan Amer <yazan.amer@progressoft.com>
2023-02-16 13:32:53 +0300

    request_processor_settings.py

--- main/processing/request_processor_settings.py
+++ main/processing/request_processor_settings.py
@@ -7,15 +7,15 @@ class RequestProcessorSettings:
         pass
 
     @abstractmethod
+    def raise_exception_if_missing(self) -> list:
+        pass
+
+    @abstractmethod
     def bikeCad_to_model_map(self) -> dict:
         pass
 
     @abstractmethod
     def keys_whose_presence_indicates_their_value(self) -> list:
-        pass
-
-    @abstractmethod
-    def raise_exception_if_missing(self) -> list:
         pass
 
     @abstractmethod
