710dacf3b2c251a09272fd1ccff814ffef926acf
Yazan Amer <yazan.amer@progressoft.com>
2023-03-01 08:16:14 +0300

    requirements.txt

---
+++ main/counterfactuals/bike_counterfactuals.py
@@ -0,0 +1,23 @@
+from main.evaluation.evaluation_service import load_pickled_predictor
+import main.processing.pandas_utility as pd_util
+
+sample_input = {'Material=Steel': -1.2089779626768866, 'Material=Aluminum': -0.46507861303022335,
+             'Material=Titanium': 1.8379997074342262, 'SSB_Include': 1.0581845284004865,
+             'CSB_Include': -0.9323228669601348, 'CS Length': -0.4947762070020683,
+             'BB Drop': 0.19327064177679704, 'Stack': -0.036955840782382385,
+             'SS E': -0.4348758585162575, 'ST Angle': 1.203226228166099, 'BB OD': -0.14197615979296274,
+             'TT OD': -0.5711431568166616, 'HT OD': -0.879229453202825, 'DT OD': -0.8924125880651749,
+             'CS OD': -0.6971543225296617, 'SS OD': -0.7226114906751929, 'ST OD': -0.8962254490159303,
+             'CS F': 0.1664798679079193, 'HT LX': -0.5559202673887266, 'ST UX': -0.5875970924732736,
+             'HT UX': -0.1666775498399638, 'HT Angle': 1.5120924379123033,
+             'HT Length': 0.7032710935570091, 'ST Length': 0.980667290296069,
+             'BB Length': -0.25473226064604454, 'Dropout Offset': -0.0325700226355687,
+             'SSB OD': -2.1985552817712657, 'CSB OD': -0.279547847307574,
+             'SSB Offset': -0.09050848378506038, 'CSB Offset': 0.5823537937924539,
+             'SS Z': -0.06959536571235439, 'SS Thickness': 0.5180142556590571,
+             'CS Thickness': 1.7994950500929077, 'TT Thickness': 0.2855204217004274,
+             'BB Thickness': -0.11934492802927218, 'HT Thickness': -0.7465363724789722,
+             'ST Thickness': -0.5700521782698762, 'DT Thickness': -1.0553146425778421,
+             'DT Length': 0.10253602811555089}
+predictor = load_pickled_predictor()
+print(predictor.predict(pd_util.get_row_from_dict(sample_input)))
---
+++ main/counterfactuals/calculate_dtai.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+
+
+def calculateDTAI(actual_performance,
+                  direction,
+                  targets,
+                  alpha_values,
+                  beta_values,
+                  smallest_allowed=1e-7) -> float:
+    alpha_values, beta_values, actual_performance = convert_to_float_arrays(alpha_values,
+                                                                            beta_values,
+                                                                            actual_performance)
+    actual = ensure_greater_than_smallest(actual_performance, smallest_allowed)
+
+    scores = calculate_scores(alpha_values, beta_values, calculate_ratios(direction, targets, actual))
+
+    sum_of_scores = tf.math.reduce_sum(scores)
+    sum_s_max = tf.math.reduce_sum(alpha_values / beta_values)
+    sum_s_min = -tf.math.reduce_sum(alpha_values)
+
+    sum_of_scores = (sum_of_scores - sum_s_min) / (sum_s_max - sum_s_min)
+    return sum_of_scores.numpy()
+
+
+def calculate_scores(alpha_values, beta_values, ratios):
+    case_less_or_equal_to_one = tf.multiply(alpha_values, ratios) - alpha_values
+    alpha_over_beta = tf.divide(alpha_values, beta_values)
+    exponential = tf.exp(tf.multiply(beta_values, (1 - ratios)))
+    case_greater_or_equal_to_one = tf.multiply(alpha_over_beta, (1 - exponential))
+    greater_than_one = tf.greater(ratios, 1)
+    greater_than_one = tf.cast(greater_than_one, "float32")
+    scores = tf.multiply(case_greater_or_equal_to_one, greater_than_one) \
+             + tf.multiply(case_less_or_equal_to_one, (1 - greater_than_one))
+    return scores
+
+
+def ensure_greater_than_smallest(actual_performance, tolerance):
+    return tf.math.maximum(actual_performance, tolerance)
+
+
+def calculate_ratios(direction, targets, y):
+    if direction == "maximize":
+        x = tf.divide(y, targets)
+    elif direction == "minimize":
+        x = tf.divide(targets, y)
+    else:
+        raise Exception("Unknown optimization direction, expected maximize or minimize")
+    return x
+
+
+def convert_to_float_arrays(alpha_values, beta_values, y_eval):
+    alpha_values = tf.cast(alpha_values, "float32")
+    beta_values = tf.cast(beta_values, "float32")
+    y_eval = tf.cast(y_eval, "float32")
+    return alpha_values, beta_values, y_eval
+
+
+if __name__ == "__main__":
+    design_index = calculateDTAI([10, 5, 3], "maximize", [6, 3, 3], [3, 3, 3], [1, 1, 1])
+    print(design_index)
