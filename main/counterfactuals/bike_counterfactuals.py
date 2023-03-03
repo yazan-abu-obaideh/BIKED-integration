003c57c0d0f62007bc44fe1119e8f03bde28019d
Yazan Amer <yazan.amer@progressoft.com>
2023-03-03 09:13:03 +0300

    dtai_counterfactuals_generator.py ready

--- main/counterfactuals/bike_counterfactuals.py
+++ main/counterfactuals/bike_counterfactuals.py
@@ -1,11 +1,6 @@
-from abc import abstractmethod, ABCMeta
-import numpy as np
-
-import dice_ml
 import pandas as pd
-from dice_ml.constants import ModelTypes
 
-from calculate_dtai import calculateDTAI
+from main.counterfactuals.dtai_counterfactuals_generator import DtaiCounterfactualsGenerator
 from main.evaluation.Predictor import Predictor
 from main.evaluation.default_processor_settings import DefaultProcessorSettings
 from main.evaluation.evaluation_service import load_pickled_predictor
@@ -35,12 +30,6 @@ sample_input = {'Material=Steel': -1.2089779626768866, 'Material=Aluminum': -0.4
                 'DT Length': 0.10253602811555089}
 
 
-class CounterfactualsPredictor(metaclass=ABCMeta):
-    @abstractmethod
-    def predict(self, data: pd.DataFrame) -> np.array:
-        pass
-
-
 class WrappedBikePredictor(Predictor):
     def __init__(self):
         self.inner_predictor = load_pickled_predictor()
@@ -49,55 +38,9 @@ class WrappedBikePredictor(Predictor):
         return self.inner_predictor.predict(data).rename(processor_settings.get_label_replacements())
 
 
-class BikeCounterfactualsGenerator:
-
-    def __init__(self,
-                 predictor: Predictor,
-                 x,
-                 y):
-        class ModelWrapper(CounterfactualsPredictor):
-            def __init__(self):
-                pass
-
-            def predict(self, _x):
-                predictions = predictor.predict(_x)
-                predictions[DTAI] = predictions.apply(BikeCounterfactualsGenerator.simple_dtai, axis=1)
-                return predictions[DTAI].values
-
-        self.x = x
-        y[DTAI] = y.apply(self.simple_dtai, axis=1)
-        design_target_index_data = y[DTAI]
-        dice_model = dice_ml.Model(model=ModelWrapper(), backend="sklearn", model_type=ModelTypes.Regressor)
-        data_for_dice = pd.concat([self.x, design_target_index_data], axis=1)
-        dice_data = dice_ml.Data(dataframe=data_for_dice,
-                                 continuous_features=list(self.x.columns.values),
-                                 outcome_name="dtai")
-
-        self.explainer = dice_ml.Dice(dice_data,
-                                      dice_model,
-                                      method="genetic")
-
-    @staticmethod
-    def simple_dtai(row):
-        list_generator = range(len(row))
-        return calculateDTAI(
-            row.values,
-            direction="maximize",
-            targets=[1 for _ in list_generator],
-            alpha_values=[1 for _ in list_generator],
-            beta_values=[4 for _ in list_generator],
-        )
-
-    def generate_counterfactuals(self):
-        return self.explainer.generate_counterfactuals(self.x[0:1],
-                                                       total_CFs=5,
-                                                       features_to_vary=[c for c in self.x.columns.values if
-                                                                         c.endswith("Thickness")],
-                                                       desired_range=[0.85, 1])
-
 bike_features, bike_predictions, _, _ = load_augmented_framed_dataset()
 
-generator = BikeCounterfactualsGenerator(WrappedBikePredictor(), bike_features, bike_predictions)
+generator = DtaiCounterfactualsGenerator(WrappedBikePredictor(), bike_features, bike_predictions)
 
 e1 = generator.generate_counterfactuals()
 e1.visualize_as_dataframe(display_sparse_df=True, show_only_changes=True)
--- main/counterfactuals/dtai_counterfactuals_generator.py
+++ main/counterfactuals/dtai_counterfactuals_generator.py
@@ -1,2 +1,62 @@
+import dice_ml
+import pandas as pd
+import numpy as np
+from dice_ml.constants import ModelTypes
+from abc import abstractmethod, ABCMeta
+from main.counterfactuals.calculate_dtai import calculateDTAI
+from main.evaluation.Predictor import Predictor
+
+DTAI = 'dtai'
+
+
+class CounterfactualsPredictor(metaclass=ABCMeta):
+    @abstractmethod
+    def predict(self, data: pd.DataFrame) -> np.array:
+        pass
+
+
 class DtaiCounterfactualsGenerator:
-    pass
+
+    def __init__(self,
+                 predictor: Predictor,
+                 x,
+                 y):
+        class ModelWrapper(CounterfactualsPredictor):
+            def __init__(self):
+                pass
+
+            def predict(self, _x):
+                predictions = predictor.predict(_x)
+                predictions[DTAI] = predictions.apply(DtaiCounterfactualsGenerator.simple_dtai, axis=1)
+                return predictions[DTAI].values
+
+        self.x = x
+        y[DTAI] = y.apply(self.simple_dtai, axis=1)
+        design_target_index_data = y[DTAI]
+        dice_model = dice_ml.Model(model=ModelWrapper(), backend="sklearn", model_type=ModelTypes.Regressor)
+        data_for_dice = pd.concat([self.x, design_target_index_data], axis=1)
+        dice_data = dice_ml.Data(dataframe=data_for_dice,
+                                 continuous_features=list(self.x.columns.values),
+                                 outcome_name="dtai")
+
+        self.explainer = dice_ml.Dice(dice_data,
+                                      dice_model,
+                                      method="genetic")
+
+    @staticmethod
+    def simple_dtai(row):
+        list_generator = range(len(row))
+        return calculateDTAI(
+            row.values,
+            direction="maximize",
+            targets=[1 for _ in list_generator],
+            alpha_values=[1 for _ in list_generator],
+            beta_values=[4 for _ in list_generator],
+        )
+
+    def generate_counterfactuals(self):
+        return self.explainer.generate_counterfactuals(self.x[0:1],
+                                                       total_CFs=5,
+                                                       features_to_vary=[c for c in self.x.columns.values if
+                                                                         c.endswith("Thickness")],
+                                                       desired_range=[0.85, 1])
