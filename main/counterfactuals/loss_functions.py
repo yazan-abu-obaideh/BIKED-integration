c7a1783f46c42762bb8360123161cbe48ef7c308
Yazan Amer <yazan.amer@progressoft.com>
2023-03-03 17:57:36 +0300

    loss_functions.py

---
+++ main/counterfactuals/loss_functions.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+
+class LossFunctionCalculator:
+    def __init__(self, dataset: pd.DataFrame):
+        self.dataset = dataset
+    def gower_distance(self, x1, x2):
+        weighted_deltas = pd.DataFrame()
+        all_columns = x1.columns.values
+        for column in all_columns:
+            weighted_deltas[column] = abs(x1[column] - x2[column]) * (1/self.get_ranges()[column])
+        return (weighted_deltas.apply(np.sum, axis=1).values[0]) * (1/len(all_columns))
+
+    def get_ranges(self):
+        ranges = {}
+        for column in self.dataset.columns.values:
+            ranges[column] = self.dataset[column].max() - self.dataset[column].min()
+        return ranges
+
---
+++ test/test_counterfactuals/test_loss_functions.py
@@ -0,0 +1,26 @@
+import unittest
+import main.processing.pandas_utility as pd_util
+from main.counterfactuals.loss_functions import LossFunctionCalculator
+import pandas as pd
+import os
+
+class LossFunctionsTest(unittest.TestCase):
+    def setUp(self) -> None:
+        with open(os.path.join(os.path.dirname(__file__), "data.csv"), "r") as file:
+            self.calculator = LossFunctionCalculator(pd.read_csv(file))
+
+    def test_gower_distance(self):
+        x1 = pd_util.get_row_from_dict({
+            "x": 5,
+            "y": 10,
+            "z": 3
+        })
+        x2 = pd_util.get_row_from_dict({
+            "x": 6,
+            "y": 10,
+            "z": 3
+        })
+        self.assertAlmostEqual(0.033,
+                               self.calculator.gower_distance(x1, x2),
+                               places=3
+                               )
