d9c391875b38a4ab3fe9688200acbf2a915f2b0f
Yazan Amer <yazan.amer@progressoft.com>
2023-03-10 11:18:19 +0300

    run_bikes_query.py

--- test/test_counterfactuals/run_bikes_query.py
+++ test/test_counterfactuals/run_bikes_query.py
@@ -1,66 +1,59 @@
-import numpy as np
 import pandas as pd
+from pymoo.core.variable import Real
 
-from main.counterfactuals.multi_objective_cfe_generator import MultiObjectiveCounterfactualsGenerator
-from main.evaluation.Predictor import Predictor
 from main.evaluation.evaluation_service import load_pickled_predictor
-from main.load_data import load_augmented_framed_dataset
+from main.counterfactuals.multi_objective_cfe_generator import MultiObjectiveCounterfactualsGenerator, CFSet
 from main.evaluation.default_processor_settings import DefaultProcessorSettings
-from pymoo.algorithms.moo.nsga2 import NSGA2
-from pymoo.optimize import minimize
+from main.load_data import load_augmented_framed_dataset
+import numpy as np
 
 x, y, _, _ = load_augmented_framed_dataset()
 settings = DefaultProcessorSettings()
+PREDICTOR = load_pickled_predictor()
 minima_found = []
+features_to_vary = ["BB OD", "TT OD", "HT OD"]
 
 
-class MyPredictor(Predictor):
+class AdaptedRegressor:
     def __init__(self):
-        self.p = load_pickled_predictor()
-        self.current_minimum = float("inf")
+        self.p = PREDICTOR
 
-    def predict(self, data: np.array) -> np.array:
-        MultiObjectiveCounterfactualsGenerator\
-            .build_from_template(None, )
-        predictions = self.p.predict((pd.DataFrame(data,
-                                                   columns=x.columns))).\
-            rename(columns=settings.get_label_replacements())
-        latest_minimum = predictions["Model Mass Magnitude"].min()
-        if latest_minimum < self.current_minimum:
-            self.current_minimum = latest_minimum
-            minima_found.append(self.current_minimum)
-            print(f"Minimum updated to {latest_minimum}")
-        return predictions.values
+    def predict(self, features):
+        model_input = MultiObjectiveCounterfactualsGenerator.build_from_template(x.iloc[0].values,
+                                                                                 np.reshape(features, (3, -1)),
+                                                                                 modifiable_indices=[
+                                                                                     x.columns.get_loc(feature)
+                                                                                     for
+                                                                                     feature in features_to_vary])
+        return self.p.predict(pd.DataFrame(model_input, columns=x.columns)) \
+            .rename(columns=DefaultProcessorSettings().get_label_replacements()).drop(columns=
+            y.columns.difference(["Model Mass Magnitude"])) \
+            .values
 
 
-PREDICTOR = MyPredictor()
+regressor = AdaptedRegressor()
 
-generator = MultiObjectiveCounterfactualsGenerator(
-    features_dataset=x,
-    predictions_dataset=y,
-    query_x=x.iloc[0:1],
-    predictor=PREDICTOR,
-    features_to_vary=["DT Thickness"],
-    query_y={"Model Mass Magnitude": [-1, 0]},
+regressor.predict(np.array([[1], [3], [5]]))
+
+x: pd.DataFrame
+y: pd.DataFrame
+
+prepared_x = x.drop(columns=x.columns.difference(features_to_vary))
+prepared_y = y.drop(columns=y.columns.difference(["Model Mass Magnitude"]))
+
+problem = MultiObjectiveCounterfactualsGenerator(
+    prepared_x,
+    prepared_y,
+    prepared_x.iloc[0:1],
+    regressor,
+    prepared_x.columns,
+    query_y={"Model Mass Magnitude": (0, 1.5)},
     bonus_objs=[],
     constraint_functions=[],
-    upper_bounds=np.array([3 for _ in range(1)]),
-    lower_bounds=np.array([-3 for _ in range(1)])
+    datatypes=[Real(-2, 2), Real(-2, 2), Real(-2, 2)]
 )
 
-algorithm = NSGA2(pop_size=1000, eliminate_duplicates=True)
-res = minimize(generator, algorithm,
-               ('n_gen', 15),
-               seed=2,
-               verbose=True)
-
-for value in res.X:
-    query = x.iloc[0:1]
-    print(f"Base value: {query['DT Thickness']}")
-    print(f"OG result: {PREDICTOR.predict(query)['Model Mass Magnitude']}")
-
-    query = pd.DataFrame(query.values, columns=query.columns.values)
-    query["DT Thickness"] = [value]
-    print(f"Modified value: {query['DT Thickness']}")
-    print(f"Modified result: {PREDICTOR.predict(query)['Model Mass Magnitude']}")
-
+cf_set = CFSet(problem, 10, 1000, initialize_from_dataset=True)
+cf_set.optimize()
+num_samples = 10
+cfs = cf_set.sample(num_samples, 1, 1, 1, 0.1, np.array([1,1]), include_dataset=True, num_dpp=2000)
