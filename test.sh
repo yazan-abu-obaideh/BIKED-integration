7625486befa07f378748737e91ddebcec4a93373
Yazan Amer <yazan.amer@progressoft.com>
2023-03-22 13:43:38 +0300

    test.sh runs all the tests

--- test.sh
+++ test.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
+echo "Running resources tests..."
+python -m unittest discover -s service_resources -t service_resources
+echo "Running unit tests"
 python -m unittest discover -s src/test -t src/test
+echo "Running e2e tests"
+python -m unittest discover -s src/app_tests -t src/app_tests
