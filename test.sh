46d857bb0510925fda9f0f6cb389735344af80a2
Yazan Amer <yazan.amer@progressoft.com>
2023-03-26 10:12:16 +0300

    test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "Running resources tests..."
 python -m unittest discover -s service_resources -t service_resources
-echo "Running unit tests"
+echo "Running unit tests..."
 python -m unittest discover -s src/test -t src/test
-echo "Running e2e tests"
+echo "Running e2e tests..."
 python -m unittest discover -s src/app_tests -t src/app_tests
