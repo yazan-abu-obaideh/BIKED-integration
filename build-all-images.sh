aca7a2852606d3219a54ef504cf395e94e7c37d0
Yazan Amer <yazan.amer@progressoft.com>
2023-08-25 11:07:26 +0300

    gateway.dockerfile and deployment / image building updates

---
+++ build-all-images.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+docker build -f gateway.dockerfile .-t jyguru/biked-integration-gateway:25aug2023
+docker build -f service-resources.dockerfile .-t jyguru/biked-integration-resources-image:25aug2023
+docker build -f service.dockerfile -t jyguru/biked-integration-beta:25aug2023
