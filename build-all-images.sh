bac1106ee57665f6a87a07b1d4dc2c19546b9df5
Yazan Amer <yazan.amer@progressoft.com>
2023-10-12 13:37:00 +0300

    update tags

--- build-all-images.sh
+++ build-all-images.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-docker build -f gateway.dockerfile . -t jyguru/biked-integration-gateway:25aug2023
-docker build -f service-resources.dockerfile . -t jyguru/biked-integration-resources-image:25aug2023
-docker build -f service.dockerfile . -t jyguru/biked-integration-beta:25aug2023
+docker build -f gateway.dockerfile . -t jyguru/biked-integration-gateway:12oct2023
+docker build -f service-resources.dockerfile . -t jyguru/biked-integration-resources-image:12oct2023
+docker build -f service.dockerfile . -t jyguru/biked-integration-beta:12oct2023
--- service.dockerfile
+++ service.dockerfile
@@ -1,5 +1,5 @@
 # syntax=docker/dockerfile:1
-FROM jyguru/biked-integration-resources-image:2023may05
+FROM jyguru/biked-integration-resources-image:12oct2023
 
 RUN apt install curl -y
 
