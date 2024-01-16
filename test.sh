5a8e4c56aa6d8fbc35673713768fb5f98427c20c
Yazan Amer <yazan.amer@progressoft.com>
2024-01-16 12:45:29 +0300

    feat: pipeline tests fix

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "Running resources tests..."
-python -m unittest discover -s service_resources -t service_resources
+python -m unittest discover -s service_resources -t service_resources || exit 1
 echo "Running unit tests..."
-python -m unittest discover -s service/test -t service/test
+python -m unittest discover -s service/test -t service/test || exit 1
 echo "Running e2e tests..."
-python -m unittest discover -s service/app_tests -t service/app_tests
+python -m unittest discover -s service/app_tests -t service/app_tests || exit 1
