92054364801da86cc9efaf36cc36c62c8eb401dc
Yazan Amer <yazan.amer@progressoft.com>
2024-01-16 12:48:42 +0300

    refactor: test.sh fix: failing test

--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
-#! /bin/bash
-echo "Running resources tests..."
-python -m unittest discover -s service_resources -t service_resources || exit 1
-echo "Running unit tests..."
-python -m unittest discover -s service/test -t service/test || exit 1
-echo "Running e2e tests..."
-python -m unittest discover -s service/app_tests -t service/app_tests || exit 1
+#!/bin/bash
+
+run_tests() {
+    echo "Running $1 tests..."
+    python -m unittest discover -s $2 -t $2 || exit 1
+}
+
+run_tests "resources" "service_resources"
+run_tests "unit" "service/test"
+run_tests "e2e" "service/app_tests"
